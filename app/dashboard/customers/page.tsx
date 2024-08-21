3427928ff8cd050c69552b4dd5ec55437a7bb321
quangnd2710 <quangnd2710@gmail.com>
2024-08-21 07:03:14 +0700

    layout page, font link

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Customers = (props: Props) => {
+  return <div>Customers</div>;
+};
+
+export default Customers;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Invoices = (props: Props) => {
+  return <div>Invoices</div>;
+};
+
+export default Invoices;
